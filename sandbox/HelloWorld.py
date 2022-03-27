c2d7185bb8c8fbf5f5d596378d9f64b48ec794c9
Christopher Michael Chandler <68424747+christopher-chandler@users.noreply.github.com>
2022-03-27 18:42:15 +0200

    update

---
+++ sandbox/HelloWorld.py
@@ -0,0 +1,98 @@
+def add(a, b):
+    '''
+    add adds two numbers together.
+    '''
+
+    res = a - b
+    return a+b
+
+def minus(a, b):
+    res = a - b
+    return res
+
+a = 2
+b = 4
+
+res = minus(a, b)
+
+resTwo = add(res, 4)
+
+# ==
+
+# True
+# False
+
+# == equal
+# != not equal
+# < less than
+# > greater than
+# <= less than or equal
+# >= greater than or equal
+# True
+# False
+
+
+
+
+
+True
+False
+
+1
+"1"
+
+a = 1
+b = "1"
+
+# bool = boolean
+list_of_numbers = [1, 2, 3, 4]
+new_list = list()
+new_list.append("Apple")
+
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# For loop
+for i in list_of_numbers:
+    print(i)
+
+
+'''
+for variable in list
+    print (variable) 
+'''
+
+# underscore
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+dogs = "bob", "mary", "John"
+
+
+# _
+# snake case
+house_of_cards = "House of cards"
+
+# Camel Case
+houseOfCards = "House of cards"
+
+houseofcards = "Hose of cards"
+
+
+
+print(dogs)
+
+
+# Hello, this is a comment.
+'''
+first line 
+second line 
+third line 
+fourth line 
+etc
+etc
+etc
+'''
+
+# etc
+
+# livereloade
+# rere
---
+++ sandbox/basicProgram.py
@@ -0,0 +1,12 @@
+import simple_functions
+
+
+res = simple_functions.add(2, 4)
+print(res)
+
+
+
+
+
+
+
